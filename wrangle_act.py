3aaafa622efc2ae1f82298c74eda50f842aba031
Mai Ahmed <Mai-CS@users.noreply.github.com>
2020-09-21 00:50:01 +0200

    Adding reports

--- wrangle_act.py
+++ wrangle_act.py
@@ -26,8 +26,13 @@ def assess(df):
 
 def clean(df, data_name):
     if data_name == DATA_NAMES[0]:
-        # Convert "timestamp" and "retweeted_status_timestamp" column from string to datetime
+        # Change data types of specific columns after filling NULL values
         df = clean_data.convert_column_to_datetime(df, "timestamp")
+        df["in_reply_to_status_id"] = df["in_reply_to_status_id"].fillna(0).astype(int)
+        df["in_reply_to_user_id"] = df["in_reply_to_user_id"].fillna(0).astype(int)
+        df["retweeted_status_id"] = df["retweeted_status_id"].fillna(0).astype(int)
+        df["retweeted_status_user_id"] = df["retweeted_status_user_id"].fillna(0).astype(int)
+        df["retweeted_status_timestamp"] = df["retweeted_status_timestamp"].fillna(0)
         df = clean_data.convert_column_to_datetime(df, "retweeted_status_timestamp")
 
         # Create new column "stage" instead of 4 columns: doggo, floofer, pupper, puppo
@@ -38,23 +43,37 @@ def clean(df, data_name):
         choices = ["doggo", "floofer", "pupper", "puppo"]
         df['stage'] = np.select(conditions, choices, default=np.NaN)
 
-        # Get specific columns from the dataset and ignore the rest
-        df = clean_data.get_some_columns(df, ["tweet_id", "stage"])
+        # Delete unwanted columns
+        df = df.drop(columns=["doggo", "floofer", "pupper", "puppo"])
 
     if data_name == DATA_NAMES[1]:
-        # Get specific columns from the dataset and ignore the rest
-        df = clean_data.get_some_columns(df, ["created_at", "id", "retweet_count", "favorite_count"])
-
         # Rename columns
         df = clean_data.rename_columns(df, {"id": "tweet_id"})
 
-        # Convert "created_at" column from string to datetime
+        # Change data types of specific columns after filling NULL values
         df = clean_data.convert_column_to_datetime(df, "created_at")
+        df["in_reply_to_status_id"] = df["in_reply_to_status_id"].fillna(0.0).astype(int)
+        df["in_reply_to_user_id"] = df["in_reply_to_user_id"].fillna(0.0).astype(int)
+        df["possibly_sensitive"] = df["possibly_sensitive"].fillna("FALSE").astype(bool)
+        df["possibly_sensitive_appealable"] = df["possibly_sensitive_appealable"].fillna("FALSE").astype(bool)
+
+        # retweeted column has False value even when retweet_count > 0, so it should be fixed
+        conditions = [df['retweet_count'] > 0]
+        choices = ["TRUE"]
+        df['retweeted'] = np.select(conditions, choices, default=np.False_)
+
+        # favorited column has False value even when favorite_count > 0, so it should be fixed
+        conditions = [df['favorite_count'] > 0]
+        choices = ["TRUE"]
+        df['favorited'] = np.select(conditions, choices, default=np.False_)
+
+        # Delete unwanted columns (having too many missing values or useless)
+        df = df.drop(
+            columns=["id_str", "in_reply_to_status_id_str", "in_reply_to_user_id_str", "in_reply_to_screen_name",
+                     "geo", "coordinates", "place", "contributors", "retweeted_status", "quoted_status_id",
+                     "quoted_status_id_str", "quoted_status"])
 
     if data_name == DATA_NAMES[2]:
-        # Get specific columns from the dataset and ignore the rest
-        df = clean_data.get_some_columns(df, ["tweet_id", "jpg_url", "p1", "p1_conf", "p1_dog"])
-
         # Rename columns
         df = clean_data.rename_columns(df, {"jpg_url": "image_url"})
 
@@ -69,8 +88,9 @@ def clean(df, data_name):
         df = clean_data.rename_columns(df, {"p1": "species_prediction",
                                             "p1_conf": "prediction_conf"})
 
-    # Remove duplicated rows
-    df = df.drop_duplicates()
+        # Delete unwanted columns (duplicated columns, columns with only one value)
+        df = df.drop(columns=["timestamp", "truncated", "is_quote_status", "possibly_sensitive",
+                              "possibly_sensitive_appealable"])
 
     # Remove rows with missing values
     df = df.dropna()
@@ -107,44 +127,60 @@ def explore_data(df):
     print("\nMost Common Dog Stage per Year:")
     print(most_popular_dog_stage)
 
-    # Create 4 subplots
-    fig, axs = plt.subplots(nrows=2, ncols=2)
-
-    # Explore correlation between variables
-    # +ve correlation means the variable increases if the other variable increases
-    # -ve correlation means the variable decreases if the other variable increases and vice versa
-    sns.heatmap(df.corr(),
-                annot=True,
-                ax=axs[0, 0])
-    axs[0, 0].set_title("Correlation Matrix")
+    # Create 2 subplots
+    fig, axs1 = plt.subplots(ncols=2)
 
     # Plot top 5 dog species
     sns.countplot(x="species_prediction",
                   data=df,
                   order=df.species_prediction.value_counts().iloc[:5].index,
-                  ax=axs[0, 1])
-    axs[0, 1].set_xticklabels(axs[0, 1].get_xticklabels(), rotation=90)
-    axs[0, 1].set_title("Top 5 Dog Species")
-    axs[0, 1].set_xlabel("Species")
-    axs[0, 1].set_ylabel("Count")
+                  ax=axs1[0])
+    axs1[0].set_xticklabels(axs1[0].get_xticklabels(), rotation=90)
+    axs1[0].set_title("Top 5 Dog Species")
+    axs1[0].set_xlabel("Species")
+    axs1[0].set_ylabel("Count")
+
+    # Plot rating for each dog species
+    sns.barplot(x="species_prediction",
+                y="rating_numerator",
+                data=df,
+                order=df.species_prediction.value_counts().iloc[:5].index,
+                ax=axs1[1])
+    axs1[1].set_title("Rating for Each Species")
+    axs1[1].set_xticklabels(axs1[1].get_xticklabels(), rotation=90)
+    axs1[1].set_xlabel("Species")
+    axs1[1].set_ylabel("Rating")
+
+    plt.tight_layout()
+
+    # Create 2 subplots
+    fig2, axs2 = plt.subplots(ncols=2)
 
     # Plot number of retweets per year
     sns.barplot(x="year",
                 y="retweet_count",
                 data=df,
-                ax=axs[1, 0])
-    axs[1, 0].set_title("Number of Retweets per Year")
-    axs[1, 0].set_xlabel("Year")
-    axs[1, 0].set_ylabel("Retweet Count")
+                ax=axs2[0])
+    axs2[0].set_title("Number of Retweets per Year")
+    axs2[0].set_xlabel("Year")
+    axs2[0].set_ylabel("Retweet Count")
 
     # Plot number of favorites for each dog stage
     sns.barplot(x="stage",
                 y="favorite_count",
                 data=df,
-                ax=axs[1, 1])
-    axs[1, 1].set_title("Favourite Count for Each Stage")
-    axs[1, 1].set_xlabel("Stage")
-    axs[1, 1].set_ylabel("Favorite Count")
+                ax=axs2[1])
+    axs2[1].set_title("Favourite Count for Each Stage")
+    axs2[1].set_xlabel("Stage")
+    axs2[1].set_ylabel("Favorite Count")
+
+    # Explore correlation between variables
+    # +ve correlation means the variable increases if the other variable increases
+    # -ve correlation means the variable decreases if the other variable increases and vice versa
+    # sns.heatmap(df.corr(),
+    #             annot=True,
+    #             ax=axs[0, 0])
+    # axs[0, 0].set_title("Correlation Matrix")
 
     plt.tight_layout()
     plt.show()
